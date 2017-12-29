72ce1af9ce6661691b986d70f1f68f43c8719f5d
angrykun <2545662859@qq.com>
2017-12-29 22:55:44 +0800

    创建SignalRChat项目

---
+++ SignalR.Solution/SignalRChat/Hub/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRChat
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name ,string message)
+        {
+            //调用 addNewMessageToPage 方法更新客户端
+            Clients.All.addNewMessageToPage(name,message);
+        }
+    }
+}
