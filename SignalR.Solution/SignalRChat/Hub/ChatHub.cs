f90c3fcb5e797b21d6e665c7bffca6b26e178b2e
angrykun <2545662859@qq.com>
2017-12-30 09:23:57 +0800

    添加注释

--- SignalR.Solution/SignalRChat/Hub/ChatHub.cs
+++ SignalR.Solution/SignalRChat/Hub/ChatHub.cs
@@ -8,6 +8,11 @@ namespace SignalRChat
 {
     public class ChatHub : Hub
     {
+        /*
+         * 派生自Hub类提供方法来构建SignalR应用程序
+         * 在Hub派生类中创建公共方法，然后通过JS访问这些方法。
+         * 在js中引用服务器类及成员应当使用小驼峰式拼写。
+         * **/
         public void Send(string name ,string message)
         {
             //调用 addNewMessageToPage 方法更新客户端
